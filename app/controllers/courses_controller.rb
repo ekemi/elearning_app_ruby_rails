4166e0eb42f6f1807a80c013a73928b268d3962f
Yapnounkie <edmundopierre@yahoo.com>
2020-12-04 02:48:54 -0800

    add set_course method

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,8 +1,15 @@
 class CoursesController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_course, only:[:show]#,#:edit, :update, :destroy]
     def index
         @courses = current_user.courses
     end
    def show 
+    set_course
    end
+
+   private
+def set_course
+    @course = current_user.courses.find(params[:id]) 
+end
 end
