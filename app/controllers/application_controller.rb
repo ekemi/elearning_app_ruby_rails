62a048fad1b0ad9e4939075f91ed11a905406311
Yapnounkie <edmundopierre@yahoo.com>
2020-12-04 04:02:14 -0800

    add errors if the page does not exist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+    def record_not_found
+        #render plain:"404 Not Found"#, status: 404
+         #redirect_to root_path
+       flash[:message] = "404 Not Found"
+       redirect_to root_path
+       
+
+
+
+    end
     protected
 
 
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,15 +1,23 @@
 class CoursesController < ApplicationController
     before_action :authenticate_user!
-    before_action :set_course, only:[:show]#,#:edit, :update, :destroy]
+    before_action :set_course, only:[:show,:edit, :update, :destroy]
     def index
         @courses = current_user.courses
     end
    def show 
     set_course
    end
-
+ def new
+ end
+ def create
+ end
+ def edit
+end
+def update
+end
    private
 def set_course
     @course = current_user.courses.find(params[:id]) 
 end
+
 end
